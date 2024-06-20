5c3d3cdc5299ce686195454eeb0aed51ad5485f5
pxassuncao <mirassuncaobjj07@gmail.com>
2024-06-20 16:37:09 -0300

    ArrumandoUseState

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -20,19 +20,48 @@ const Formulario = (props) =>{
     const [email, setEmail] = useState('')
     const[time,setTime] = useState('')
 
-    const aoSalvar=(evento)=>{
+    const aoSalvar= (evento) =>{
         evento.preventDefault()
-        console.log('Formulário foi submetido =>', nome, cargo, email, time)
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            email,
+            time
+        })
+        
     }
 
     return(
         <section className="formulario">
             <form onSubmit={aoSalvar}>
             <h2>Preencha os dados para criar o card do programador</h2>
-            <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" valor={nome} aoAlterado={varlor =>setNome(valor)}/>
-            <CampoTexto obrigatorio={true} label="Cargo" placeholder= "Digite o seu cargo" valor={cargo} aoAlterado={varlor =>setCargo(valor)}/>
-            <CampoTexto obrigatorio={true} label="Email" placeholder="Digite o endereço de email" valor={email} aoAlterado={varlor =>setEmail(valor)}/>
-            <ListaSuspensa obrigatorio={true} label ="Especialidade" itens = {times}valor={time} aoAlterado={varlor =>setTime(valor)}/>
+            <CampoTexto
+             obrigatorio={true} 
+             label="Nome" 
+             placeholder="Digite seu nome" 
+             valor={nome} 
+             aoAlterado={valor=>setNome(valor)}
+             />
+            <CampoTexto
+             obrigatorio={true} 
+             label="Cargo" 
+             placeholder= "Digite o seu cargo" 
+             valor={cargo} 
+             aoAlterado={valor =>setCargo(valor)}
+             />
+            <CampoTexto
+             obrigatorio={true} 
+             label="Email" 
+             placeholder="Digite o endereço de email" 
+             valor={email} 
+             aoAlterado={valor =>setEmail(valor)}/>
+            <ListaSuspensa 
+            obrigatorio={true} 
+            label ="Especialidade" 
+            itens = {times}
+            valor={time} 
+            aoAlterado={valor =>setTime(valor)}
+            />
             <Botao>
                 Criar Card
             </Botao>
